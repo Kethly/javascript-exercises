b7ddb50fa7c5ad6343269e188be94b957f2b26e6
Kethly <Kethlyfast@gmail.com>
2022-05-17 23:21:30 -0400

    finished basic odin project practice questions

--- 13_caesar/caesar.js
+++ 13_caesar/caesar.js
@@ -1,5 +1,18 @@
-const caesar = function() {
+const caesar = function(str, shift) {
+    let temp = "";
+    let newShift = shift < 0 ? 26 + shift : shift;
+    for(let x = 0; x < str.length; x++){
+        let val = str.charCodeAt(x);
+        if((val <= 90 && val >= 65) || (val <= 122 && val >= 97)){
+          let upper = (+(val > 90) * 32) + 65;
 
+        temp += String.fromCharCode(((val - upper + newShift) % 26) + upper);}
+        else {
+          temp += str.charAt(x);
+        }
+
+    }
+    return temp;
 };
 
 // Do not edit below this line
